39d23a8ba8275180076ea0ec5c26316bf0be35bd
Ayaka Ohigashi <a.ohigashi.0129@gmail.com>
2023-12-09 23:05:48 +0900

    fix code

--- JStask1_5.js
+++ JStask1_5.js
@@ -1,30 +1,32 @@
 'use strict';
 
-document.querySelector("form").addEventListener("submit", (e) => {
+document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const FizzNum = document.getElementById("Fizz").value;
-  const BuzzNum = document.getElementById("Buzz").value;
+  const FizzNum = document.getElementById('Fizz').value;
+  const BuzzNum = document.getElementById('Buzz').value;
 
   if ((FizzNum % 1 === 0) && (BuzzNum % 1 === 0) && (FizzNum !== '') && (BuzzNum !== '')) {
 
     const ul = document.querySelector('ul');
 
-    for (let int = 1; int <= 99; int++) {
+    const addList = (text, ul) => {
       const li = document.createElement('li');
+      li.textContent = text;
+      ul.appendChild(li);
+    }
 
+    for (let int = 1; int <= 99; int++) {
       if ((int % FizzNum === 0) && (int % BuzzNum === 0)) {
-        li.textContent = 'FizzBuzz ' + int;
-        ul.appendChild(li);
+        addList('FizzBuzz ' + int, ul);
       } else if (int % FizzNum === 0) {
-        li.textContent = 'Fizz ' + int;
-        ul.appendChild(li);
+        addList('Fizz ' + int, ul);
       } else if (int % BuzzNum === 0) {
-        li.textContent = 'Buzz ' + int;
-        ul.appendChild(li);
+        addList('Buzz ' + int, ul);
       }
     }
   } else {
-    console.log("整数を入力してください");
+    console.log('整数を入力してください');
   }
 });
+
